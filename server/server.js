929920ceba8119036b9745eb5d720d4341d83e4d
nallaperumal007 <nallaperumal342@gmail.com>
2023-12-27 18:33:12 +0530

    server create simple routing

--- server/server.js
+++ server/server.js
@@ -1,1 +1,10 @@
-console.log("Hello world!!");
+const express=require('express');
+const router=require('./routes/routes.js');
+
+const app=express();
+
+app.use('/',router)
+app.listen(8000,err=>{
+    if(err) console.log(err)
+    console.log('Server is started at port number:8000')
+})
