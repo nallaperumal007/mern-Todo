d80d6361f6417b00297876b8858af31706c8fe50
nallaperumal007 <nallaperumal342@gmail.com>
2023-12-27 22:59:58 +0530

    create crud operation and ui creation is finish

--- client/src/Components/Todolist.jsx
+++ client/src/Components/Todolist.jsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import './Todolist.css';
-
-export default function Todolist(props) {
-  const todolist=props.todolist.map((task,index) =>  {
-    return <li key={index}>
-           <div>
-
-           </div>
-           <div>
-
-           </div>
-
-          </li>
-  })
-
-  return (
-    <div className='tasklist'>
-      <ul>
-        {todolist}
-      </ul>
-    </div>
-  )
+import './Todolist.css'
+import React from 'react'
+import CheckIcon from '@mui/icons-material/Check';
+import EditIcon from '@mui/icons-material/Edit';
+import CloseIcon from '@mui/icons-material/Close';
+import axios from 'axios'
+function Todolist(props) {
+    const todolist = props.todolist.map((task,index) => {
+        const taskComplete = task => {
+            axios.put(`http://localhost:8000/${task._id}` , {
+                _id : task._id,
+                todo: task.todo,
+                isComplete : !task.isComplete
+            }).then(res => props.taskComplete(res.data)).catch(err => console.log(err))
+        }
+        const removeTask = id => {
+            axios.delete(`http://localhost:8000/${id}`).then(res => props.removeTask(res.data)).catch(err => console.log(err))
+        } 
+        return <li key = {index}>
+            <div style = {{display : 'flex'}}>
+               <CheckIcon className = {task.isComplete ? 'isComplete' : 'checkicon'}/>
+               <p className = {task.isComplete ? 'taskcomplete' : ''} onClick = {() => {
+                   taskComplete(task)
+               }}>{task.todo}</p>
+            </div>
+            <div>
+                <EditIcon className = 'edit' onClick = {() => {
+                    props.tasktoUpdate(task)
+                    props.showPopup()
+                }}/>
+                <CloseIcon className = 'close' onClick = {() => {
+                    removeTask(task._id)
+                }}/>
+            </div>
+        </li>
+    })
+    return (
+        <div className = 'tasklist'>
+            <ul>
+                {todolist}
+            </ul>
+        </div>
+    )
 }
+
+export default Todolist
